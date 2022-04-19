d7162ee41ef2e33ddeac9682c3f2f7dd32b579f1
Maria Ana Marraccini <mariaanamarraccini@gmail.com>
2022-04-19 18:21:05 -0300

    BaSP-W05-2022: ex-01-variables_and_operators/making the exercise full English

--- Semana-05/exercises/01_variables_and_operators.js
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -1,7 +1,7 @@
 console.log('--EXERCISE 1: VARIABLES AND OPERATORS')
 
-//a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos 
-//números en una 3er variable.
+//a. Create two numeric variables and use the addition operator to store the value of the sum of both
+//numbers in a 3rd variable.
 
 console.log('Excercise 1.a:')
 var number1 = 50;
@@ -9,7 +9,7 @@ var number2 = 20;
 var result = number1 + number2;
 console.log('result: ' + number1 + '+' + number2 + '=' + result); 
 
-//b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+//b. Create two variables of type String and concatenate them saving the result in a 3rd variable.
 
 console.log('Exercise 1.b')
 var string1 = 'I am';
@@ -17,8 +17,8 @@ var string2 = 'Maria Ana';
 var result = string1 + " " + string2;
 console.log('result: ' + result);
 
-//c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la 
-//suma en una 3er variable (utilizar length).
+//c. Create two variables of type String and add the length of each variable (number of letters in the string) saving the result of the
+//sum in a 3rd variable (use length).
 
 console.log('Exercise 1.c')
 var string1 = "Radium";
