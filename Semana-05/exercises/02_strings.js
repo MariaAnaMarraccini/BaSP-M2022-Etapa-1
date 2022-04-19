5dba34cda44491feec31c4d01aceeb75e0d3bb56
Maria Ana Marraccini <mariaanamarraccini@gmail.com>
2022-04-19 18:15:27 -0300

    BaSP-W05-2022: ex-02-strings/making the words in English

--- Semana-05/exercises/02_strings.js
+++ Semana-05/exercises/02_strings.js
@@ -1,49 +1,51 @@
 console.log('--EXERCISE 2: STRINGS')
 
-//a.Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase).
+//a.Create a variable of type string with at least 10 characters and convert all text to uppercase (use toUpperCase).
 
 console.log('Exercise 2.a.')
 var stringWord = 'maria ana';
 stringWord = stringWord.toLocaleUpperCase();
 console.log('result: ' + stringWord);
 
-//b. Crear una variable de tipo string con al menos 10 caracteres
-//y generar un nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).
+//b. Create a variable of type string with at least 10 characters
+//and generate a new string with the first 5 characters saving the result in a new variable (use substring).
 
 console.log('Exercise 2.b.');
-var stringWord = 'inglaterra';
+var stringWord = 'Kazakhstan';
 var stringWord2 = stringWord.substring(0,5);
 console.log('result: ' + 'The first five characters of ' + stringWord + ' are ' + stringWord2);
 
-//c. Crear una variable de tipo string con al menos 10 caracteres y 
-//generar un nuevo string con los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).
+//c. Create a variable of type string with at least 10 characters and
+//generate a new string with the last 3 characters saving the result in a new variable (use substring).
 
 console.log('Exercise 2.c.');
-var stringWord = 'inglaterra';
+var stringWord = 'Luxembourg';
 var stringWord2 = stringWord.substring(7,10);
 console.log('result: ' + 'The last three caracters of ' + stringWord + ' are ' + stringWord2);
 
-//d. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y 
-//las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).
+//d. Create a variable of type string with at least 10 characters and generate a new string with the first letter in uppercase and
+// the rest in lowercase. Save the result in a new variable (use substring, toUpperCase, toLowerCase and the + operator).
 
 console.log('Exercise 2.d.');
-var stringWord = 'inglaterra';
+var stringWord = 'bangladesh';
 var stringWord2 = stringWord.substring(0,1).toUpperCase() + stringWord.substring(1,10).toLowerCase();
 console.log('result: ' + stringWord2);
 
-//e. Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. 
-//Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).
+//e. Create a variable of type string with at least 10 characters and some white space.
+//Find the position of the first whitespace and save it in a variable (use indexOf).
 
 console.log('Exercise 2.e.');
 var word = 'Maria Ana';
 var spacePosition = word.indexOf(' ');
 console.log('result: the space in the var ' + word + ' is in character number ' + spacePosition);
 
-//f.Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). 
-//Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de 
-//ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).
+//f.Create a variable of type string with at least 2 words long (10 characters and some space in between).
+//Use the methods from the previous exercises to generate a new string that has the first letter of
+//both words capitalized and all other letters lowercase (use indexOf, substring, toUpperCase, toLowerCase, and the + operator).
 
 console.log('Exercise 2.f.');
-var twoWords = 'termo yerba';
-var result = twoWords.substring(twoWords.indexOf('t'),1).toUpperCase() + twoWords.substring(1,5).toLowerCase()+ twoWords.substring(twoWords.indexOf(' '),6) + twoWords.substring(twoWords.indexOf('y'),7).toUpperCase() +  twoWords.substring(7,11).toLowerCase();
+var twoWords = 'tjken adopt';
+var result = twoWords.substring(twoWords.indexOf('t'),1).toUpperCase() + twoWords.substring(1,5).toLowerCase()+ 
+twoWords.substring(twoWords.indexOf(' '),6) + twoWords.substring(twoWords.indexOf('a'),7).toUpperCase() +  
+twoWords.substring(7,11).toLowerCase();
 console.log('result: ' + result);
